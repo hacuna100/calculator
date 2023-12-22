f47760602469e4cd911d50155a2d17bb5c9b1ce3
Hermilo Acuna <hacuna100@gmail.com>
2023-12-22 15:26:20 -0700

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'calculator';
+  one = 1;
+  two = 2;
+  three = 3;
+  four = 4;
+  five = 5;
+  six = 6;
+  seven = 7;
+  eight = 8;
+  nine = 9;
+
+
+  calc() {
+    
+
+  }
+
+
 }
